03450c32d1c763c1ba166a11202c65ee6589ece8
pacmanzou <3339132700@qq.com>
2020-08-16 21:13:26 +0800

    fix

--- gruvbox.vim
+++ gruvbox.vim
@@ -1,14 +1,14 @@
 function! crystalline#theme#gruvbox#set_theme() abort
     call crystalline#generate_theme({
-          \ 'NormalMode':  [[246, 239], ['#EBDBB2', '#45413b']],
-          \ 'InsertMode':  [[235, 109], ['#8be9fd', '#282828']],
-          \ 'VisualMode':  [[235, 208], ['#FAD000', '#282828']],
-          \ 'ReplaceMode': [[235, 108], ['#00FF00', '#282828']],
-          \ '':            [[246, 239], ['#EBDBB2', '#45413b']],
-          \ 'Inactive':    [[246, 239], ['#EBDBB2', '#45413b']],
-          \ 'Fill':        [[246, 239], ['#EBDBB2', '#45413b']],
+          \ 'NormalMode':  [[246, 239], ['#EBDBB2', '#3c3836']],
+          \ 'VisualMode':  [[246, 239], ['#EBDBB2', '#3c3836']],
+          \ 'InsertMode':  [[246, 239], ['#EBDBB2', '#3c3836']],
+          \ 'ReplaceMode': [[246, 239], ['#EBDBB2', '#3c3836']],
+          \ '':            [[246, 239], ['#EBDBB2', '#3c3836']],
+          \ 'Inactive':    [[246, 239], ['#EBDBB2', '#3c3836']],
+          \ 'Fill':        [[246, 239], ['#EBDBB2', '#3c3836']],
           \ 'Tab':         [[246, 239], ['#a89984', '#504945']],
-          \ 'TabType':     [[246, 239], ['#EBDBB2', '#45413b']],
+          \ 'TabType':     [[246, 239], ['#EBDBB2', '#3c3836']],
           \ 'TabSel':      [[235, 246], ['#282828', '#a89984']],
           \ 'TabFill':     [[235, 235], ['#00FFFFFF', '#00FFFFFF']],
           \ })
--- ranger/commands.py
+++ ranger/commands.py
@@ -9,6 +9,36 @@ class paste_as_root(Command):
 		else:
 			self.fm.execute_console('shell sudo cp -r %c .')
 
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --reverse --header='Jump to file'"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --reverse --header='Jump to filemap <C-f> fzf_select'"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
 class compress(Command):
     def execute(self):
         """ Compress marked files to current directory """
